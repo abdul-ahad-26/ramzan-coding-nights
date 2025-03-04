8c2241cecf63fa48ce49133d3072725affe11e75
abdul-ahad-26 <hayymalik678@gmail.com>
2025-03-05 00:54:32 +0500

    put text of plan to readme

---
+++ unit-converter/README.md
@@ -0,0 +1,4 @@
+# 1. User will enter the value
+# 2. select from unit
+# 3. select to unit
+# 4. Converted value from a created function will be shown 
--- unit-converter/unit-converter.py
+++ unit-converter/unit-converter.py
@@ -1,37 +1,36 @@
+# Imports
 import streamlit as st
 
-# 1. User will enter the value
-# 2. select from unit
-# 3. select to unit
-# 4. Converted value from a created function will be shown 
 
 def convert_units(value, unit_from, unit_to):
-    
-    conversions={
-        "meter_to_kilometer":0.001,
-        "kilometer_to_meter":1000,
-        "gram_to_kilogram":0.001,
-        "kilogram_to_gram":1000,
-        "foot_to_inch":12,
-        "inch_to_foot":0.0833333,
-        "foot_to_meter":0.3048,
-        "meter_to_foot":3.28084
+
+    conversions = {
+        "meter_to_kilometer": 0.001,
+        "kilometer_to_meter": 1000,
+        "gram_to_kilogram": 0.001,
+        "kilogram_to_gram": 1000,
+        "foot_to_inch": 12,
+        "inch_to_foot": 0.0833333,
+        "foot_to_meter": 0.3048,
+        "meter_to_foot": 3.28084,
     }
 
-    key=f"{unit_from}_to_{unit_to}"
+    key = f"{unit_from}_to_{unit_to}"
 
     if key in conversions:
         return value * conversions[key]
     else:
         return "Conversion not supported"
-    
+
 
 st.title("Unit Coverter 🔧 ")
-value = st.number_input("Enter the value",min_value=1.0,step=1.0)
-unit_from = st.selectbox("Convert from",["kilometer","meter","gram","kilogram","foot","inch"])
-unit_to = st.selectbox("Convert to",["meter","kilometer","gram","kilogram","foot","inch"]
- ) 
+value = st.number_input("Enter the value", min_value=1.0, step=1.0)
+unit_from = st.selectbox(
+    "Convert from", ["kilometer", "meter", "gram", "kilogram", "foot", "inch"]
+)
+unit_to = st.selectbox(
+    "Convert to", ["meter", "kilometer", "gram", "kilogram", "foot", "inch"]
+)
 
-result = convert_units(value,unit_from, unit_to)
+result = convert_units(value, unit_from, unit_to)
 st.success(f"Converted value: {result}")
-
