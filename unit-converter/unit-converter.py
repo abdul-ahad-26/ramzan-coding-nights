c681e0ea241d8aeb6b68542a494c779a94c0a08b
abdul-ahad-26 <hayymalik678@gmail.com>
2025-03-05 00:53:02 +0500

    added more units

--- unit-converter/unit-converter.py
+++ unit-converter/unit-converter.py
@@ -11,7 +11,11 @@ def convert_units(value, unit_from, unit_to):
         "meter_to_kilometer":0.001,
         "kilometer_to_meter":1000,
         "gram_to_kilogram":0.001,
-        "kilogram_to_gram":1000
+        "kilogram_to_gram":1000,
+        "foot_to_inch":12,
+        "inch_to_foot":0.0833333,
+        "foot_to_meter":0.3048,
+        "meter_to_foot":3.28084
     }
 
     key=f"{unit_from}_to_{unit_to}"
@@ -22,12 +26,12 @@ def convert_units(value, unit_from, unit_to):
         return "Conversion not supported"
     
 
-st.title("Unit Coverter")
+st.title("Unit Coverter 🔧 ")
 value = st.number_input("Enter the value",min_value=1.0,step=1.0)
-unit_from = st.selectbox("Convert from", ["meter","kilometer","gram","kilogram"])
-unit_to = st.selectbox("Convert to", ["meter","kilometer","gram","kilogram"])
+unit_from = st.selectbox("Convert from",["kilometer","meter","gram","kilogram","foot","inch"])
+unit_to = st.selectbox("Convert to",["meter","kilometer","gram","kilogram","foot","inch"]
+ ) 
 
-if st.button("Convert"):
-    result = convert_units(value,unit_from, unit_to)
-    st.write(f"Converted value: {result}")
+result = convert_units(value,unit_from, unit_to)
+st.success(f"Converted value: {result}")
 
