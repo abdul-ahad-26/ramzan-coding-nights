32f459f6b814f67dd41a2505d954179eba3b63f8
abdul-ahad-26 <hayymalik678@gmail.com>
2025-03-22 01:03:20 +0500

    upload stateful chatbot with authentication

---
+++ qa-chatbot/terminal.py
@@ -0,0 +1,24 @@
+# Imports
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+while True:
+    user_input = input("\nEnter your question (or 'quit' to exit): ")
+
+    if user_input == "quit":
+        print("Thankyou for chatting! Goodbye!")
+        break
+
+    response = model.generate_content(user_input)
+
+    print(response.text)
+
+
+
